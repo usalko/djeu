4e5770fc36387fdab49f96c194569d698a290c3a
Vanya U <ivict@rambler.ru>
2022-12-24 21:48:24 +0300

    # There is the fix for coordinates

--- complex-components/pdf-viewer/src/components/AreaHighlight.tsx
+++ complex-components/pdf-viewer/src/components/AreaHighlight.tsx
@@ -19,46 +19,43 @@ export class AreaHighlight extends Component<Props> {
     const { highlight, onChange, locked, selected, ...otherProps } = this.props;
 
     return (
-      <div
-        className={`AreaHighlight ${locked ? "AreaHighlight--locked" : ""} ${selected ? "AreaHighlight--selected" : ""}`}
-      >
-        <Rnd
-          className="AreaHighlight__part"
-          onDragStop={(_, data) => {
-            const boundingRect: LeftTopWidthHeightPageNumber = {
-              ...highlight.position.boundingRect,
-              top: data.y - 20,
-              left: data.x,
-            }
+      <Rnd
+        allowAnyClick={!locked}
+        className={`AreaHighlight__part ${locked ? "AreaHighlight--locked" : ""} ${selected ? "AreaHighlight--selected" : ""}`}
+        onDragStop={(_, data) => {
+          const boundingRect: LeftTopWidthHeightPageNumber = {
+            ...highlight.position.boundingRect,
+            top: data.y,
+            left: data.x,
+          }
 
-            onChange(boundingRect);
-          }}
-          onResizeStop={(_mouseEvent, _direction, ref, _delta, position) => {
-            const boundingRect: LeftTopWidthHeightPageNumber = {
-              top: position.y - 20,
-              left: position.x,
-              width: ref.offsetWidth,
-              height: ref.offsetHeight,
-              pageNumber: getPageFromElement(ref)?.number || -1,
-            }
+          onChange(boundingRect);
+        }}
+        onResizeStop={(_mouseEvent, _direction, ref, _delta, position) => {
+          const boundingRect: LeftTopWidthHeightPageNumber = {
+            top: position.y,
+            left: position.x,
+            width: ref.offsetWidth,
+            height: ref.offsetHeight,
+            pageNumber: getPageFromElement(ref)?.number || -1,
+          }
 
-            onChange(boundingRect)
-          }}
-          position={{
-            x: highlight.position.boundingRect.left,
-            y: highlight.position.boundingRect.top - 20,
-          }}
-          size={{
-            width: highlight.position.boundingRect.width,
-            height: highlight.position.boundingRect.height,
-          }}
-          onClick={(event: Event) => {
-            event.stopPropagation();
-            event.preventDefault();
-          }}
-          {...otherProps}
-        />
-      </div>
+          onChange(boundingRect)
+        }}
+        position={{
+          x: highlight.position.boundingRect.left,
+          y: highlight.position.boundingRect.top,
+        }}
+        size={{
+          width: highlight.position.boundingRect.width,
+          height: highlight.position.boundingRect.height,
+        }}
+        onClick={(event: Event) => {
+          event.stopPropagation();
+          event.preventDefault();
+        }}
+        {...otherProps}
+      />
     );
   }
 }
